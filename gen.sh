e57ad1609351b97599ea1f1812470e8423d5056a
pexcn <i@pexcn.me>
2019-08-02 22:48:16 +0800

    update script

--- gen.sh
+++ gen.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
+# date tag
+cat << EOF > blacklist.conf
+#
+# Update: $(date +'%Y-%m-%d %T')
+#
+
+EOF
+
 # blacklist
-sed '/^[[:space:]]*$/d' blacklist.txt | sed -e '/^#/!s/^/server=\//g' -e '/^#/!s/$/\//g' > blacklist.conf
+cat << EOF >> blacklist.conf
+#
+# Block the harmful domains
+#
+
+EOF
+sed '/^[[:space:]]*$/d' blacklist.txt | sed -e '/^#/!s/^/server=\//g' -e '/^#/!s/$/\//g' >> blacklist.conf
+
+echo >> blacklist.conf
+echo >> blacklist.conf
+echo >> blacklist.conf
+
 # blacklist-exclude
-sed '/^[[:space:]]*$/d' blacklist-exclude.txt | sed -e '/^#/!s/^/server=\//g' -e '/^#/!s/$/\/114.114.114.114/g' > blacklist-exclude.conf
+cat << EOF >> blacklist.conf
+#
+# Exclude some subdomains
+#
+
+EOF
+sed '/^[[:space:]]*$/d' blacklist-exclude.txt | sed -e '/^#/!s/^/server=\//g' -e '/^#/!s/$/\/114.114.114.114/g' >> blacklist.conf
